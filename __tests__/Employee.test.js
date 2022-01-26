f5a2f4dbf7001fb2c03c90a52516f996fa73c816
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-26 14:34:33 -0800

    employee and tests done

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,43 @@
 const Employee = require('../lib/Employee.js');
 
-test('creates an employee object', () => {
-    const employee = new Employee('Bob');
-  
-    expect(employee.name).toBe('Bob');
-    expect(employee.email).toEqual(expect.any(Number));
-  });
+test("return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Bob", 1, "bob@email.com", "Employee");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test('create name', () => {
+    const name = "Bob";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("get name", () => {
+    const testValue = "Bob";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("create id", () => {
+    const testValue = 1;
+    const e = new Employee("Bob", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("get id", () => {
+    const testValue = 1;
+    const e = new Employee("Bob", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test ("create email", () => {
+    const testValue = "bob@email.com";
+    const e = new Employee("Bob", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("get email", () => {
+    const testValue = "bob@email.com";
+    const e = new Employee("Bob", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
